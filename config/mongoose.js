99c7479c0ed9d19fd30e73b55bdc7627da731ad3
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-02-20 14:20:10 +0200

    db name

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const { DB_URI } = require('./config');
+// const { DB_URI } = require('./config');
 
 // const dbOptions = {
 //     useNewUrlParser: true, 
@@ -17,15 +17,27 @@ const { DB_URI } = require('./config');
 // db.on('error', console.error.bind(console, 'DB connection error:'));
 // db.once('open', console.log.bind(console, 'DB is connected'))
 
-const db = async () => {
+// const db = async () => {
+//     try {
+//         mongoose.connect(DB_URI);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// module.exports = db;
+
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
     try {
-        mongoose.connect(DB_URI);
-    } catch (error) {
-        console.log(error);
+        await mongoose.connect(process.env.DATABASE_URI)
+    } catch (err) {
+        console.log(err)
     }
 }
 
 mongoose.connection.on('error', console.error.bind(console, 'DB connection error:'));
-mongoose.connection.once('open', console.log.bind(console, 'DB is connected'))
+mongoose.connection.once('open', console.log.bind(console, 'DB is connected'));
 
-module.exports = db;
+module.exports = connectDB;
