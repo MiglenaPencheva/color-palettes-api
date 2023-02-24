c2fa66111eed5072bbb7db7333e53ab26b08b2d8
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-02-24 15:05:24 +0200

    Update colorPalettesController.js

--- controllers/colorPalettesController.js
+++ controllers/colorPalettesController.js
@@ -8,7 +8,7 @@ const preload = require('../middleware/preload');
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads');
+        cb(null, '/uploads');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '--' + file.originalname);
