00a61237434389a3d1d15dce54cd11de651b6eb8
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-02-25 16:56:50 +0200

    Update colorPalettesController.js

--- controllers/colorPalettesController.js
+++ controllers/colorPalettesController.js
@@ -72,7 +72,7 @@ router.post('/', isAuth(), upload.single('imageFile'), async (req, res, next) =>
         const file = req.file.path;
         if (file == '') throw { message: 'Image is required' }
         // imageFile = 'http://localhost:5500/' + file;
-        imageFile = 'https://megacolormix.onrender.com/' + file;
+        imageFile = 'https://coloralettes-api.onrender.com/' + file;
         
         // const item = { title, category, colors,  };
         const item = { title, category, colors, imageFile };
