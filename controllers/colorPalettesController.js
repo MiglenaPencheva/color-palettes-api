86117550aafea9545d7c1dd5321d41e8c85b2dd8
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-02-26 22:07:17 +0200

    file changes

--- controllers/colorPalettesController.js
+++ controllers/colorPalettesController.js
@@ -70,13 +70,15 @@ router.post('/', isAuth(), upload.single('imageFile'), async (req, res, next) =>
         if (colors == '') throw { message: 'Choose at least one color' }
 
         const file = req.file.path;
+        console.log(file);
+        console.log(file.name);
         if (file == '') throw { message: 'Image is required' }
         // imageFile = 'http://localhost:5500/' + file;
-
-        // imageFile = 'https://coloralettes-api.onrender.com/' + file;
+        // const imageFile = 'https://coloralettes-api.onrender.com/' + file;
         
-        // const item = { title, category, colors,  };
-        const item = { title, category, colors, imageFile: file };
+        const imageFile = 'iiimmmaaagggeee'
+
+        const item = { title, category, colors, imageFile };
         item.likedBy = [];
         item.creator = req.user._id;
 
