4bdbf269cb175b236f3bec3b5724f9cbe57f737f
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-03-06 22:09:53 +0200

    url fixed

--- controllers/colorPalettesController.js
+++ controllers/colorPalettesController.js
@@ -75,7 +75,7 @@ router.post('/', isAuth(), upload.single('imageFile'), async (req, res, next) =>
 
         if (file == '') throw { message: 'Image is required' }
         // imageFile = 'http://localhost:5500/' + file;
-        imageFile = 'https://coloralettes-api.onrender.com/' + file;
+        imageFile = 'https://colorpalettes-api.onrender.com/' + file;
 
         const item = { title, category, colors, imageFile };
         item.likedBy = [];
