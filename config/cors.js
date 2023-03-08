a553b13e1b75436973a4fa3708e32e350b489594
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-03-08 14:11:01 +0200

    cors fixed

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://megacolormix.onrender.com, localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', ['https://megacolormix.onrender.com', 'localhost:3000']);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization, Authorization, x-authorization');
     next();
