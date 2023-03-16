7571ed05a09fcbae072050cf9375f7effebe3a4a
Miglena Pencheva <miglena_pencheva@abv.bg>
2023-03-16 14:34:51 +0200

    fix memory issue

--- services/colorPalettesService.js
+++ services/colorPalettesService.js
@@ -4,6 +4,7 @@ async function getAll(query) {
     return await ColorPalette
         .find({ title: { $regex: query || '', $options: 'i' } })
         .sort({ 'created_at': -1 })
+        .allowDiskUse(true)
         .lean();
 }
 
